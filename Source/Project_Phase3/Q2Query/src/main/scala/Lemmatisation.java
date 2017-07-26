40b8ff9d35b4d73e1882e9abcfe9e27d10bc8934
ROHITHKUMARN <rnd95@mail.umkc.edu>
2017-07-25 22:32:54 -0500

    Phase 3

---
+++ Source/Project_Phase3/Q2Query/src/main/scala/Lemmatisation.java
@@ -0,0 +1,33 @@
+
+import edu.stanford.nlp.simple.Document;
+import edu.stanford.nlp.simple.Sentence;
+import java.util.List;
+
+/**
+ * Created by rohithkumar on 7/15/17.
+ */
+
+public class Lemmatisation {
+
+    public static String returnLemma(String sentence) {
+
+        System.out.println("sentence="+sentence);
+
+        Document doc = new Document(sentence);
+
+        String lemma="";
+
+        for (Sentence sent : doc.sentences()) {
+            List<String> l=sent.lemmas();
+            for (int i = 0; i < l.size() ; i++) {
+                lemma+= l.get(i) +" ";
+            }
+        }
+        return lemma;
+    }
+//    public static void main(String args[]) {
+//        String s = "usage is giving good example of lemmatisations";
+//        String le= Lemmatisation.returnLemma(s);
+//        System.out.println("lemmatised="+le) ;
+//    }
+}
